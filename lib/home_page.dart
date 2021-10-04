d60268f1272781d12518fd89e3663f22985f79b8
matsumo-and <yh134.toisanda@gmail.com>
2021-10-04 23:28:38 +0900

    地図を表示できた

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,17 +3,9 @@ import 'package:location/location.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 
 Future<LatLng> acquireCurrentLocation() async {
-  // Initializes the plugin and starts listening for potential platform events
   Location location = Location();
-
-  // Whether or not the location service is enabled
   bool serviceEnabled;
-
-  // Status of a permission request to use location services
   PermissionStatus permissionGranted;
-
-  // Check if the location service is enabled, and if not, then request it. In
-  // case the user refuses to do it, return immediately with a null result
   serviceEnabled = await location.serviceEnabled();
   if (!serviceEnabled) {
     serviceEnabled = await location.requestService();
@@ -21,11 +13,6 @@ Future<LatLng> acquireCurrentLocation() async {
       return LatLng(0, 0);
     }
   }
-
-  // Check for location permissions; similar to the workflow in Android apps,
-  // so check whether the permissions is granted, if not, first you need to
-  // request it, and then read the result of the request, and only proceed if
-  // the permission was granted by the user
   permissionGranted = await location.hasPermission();
   if (permissionGranted == PermissionStatus.denied) {
     permissionGranted = await location.requestPermission();
@@ -34,7 +21,6 @@ Future<LatLng> acquireCurrentLocation() async {
     }
   }
 
-  // Gets the current location of the user
   final locationData = await location.getLocation();
   return LatLng(
       locationData.latitude!.toDouble(), locationData.longitude!.toDouble());
@@ -44,46 +30,37 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final String token =
-        'sk.eyJ1IjoicG9ueWFvIiwiYSI6ImNrdWNoeW1xNzEwbHcybm12eXhxZzZrcDAifQ.EcubtKjMoz3mMPWLbLoSGw';
+        'pk.eyJ1IjoicG9ueWFvIiwiYSI6ImNrdHo3bGZpMDA3MzUyb25wbHRlYW8zZjcifQ.zJp2XScgvelBiKISsrs5hQ';
     final String style = 'mapbox://styles/ponyao/cktznbqp322fe17pcoozkalaj';
 
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Map"),
+      ),
       body: MapboxMap(
           accessToken: token,
           styleString: style,
           initialCameraPosition: CameraPosition(
             zoom: 15.0,
-            target: LatLng(14.508, 46.048),
+            target: LatLng(35.15362, 136.96964),
           ),
-
-          // The onMapCreated callback should be used for everything related
-          // to updating map components via the MapboxMapController instance
           onMapCreated: (MapboxMapController controller) async {
-            // Acquire current location (returns the LatLng instance)
             final result = await acquireCurrentLocation();
 
-            // You can either use the moveCamera or animateCamera, but the former
-            // causes a sudden movement from the initial to 'new' camera position,
-            // while animateCamera gives a smooth animated transition
             await controller.animateCamera(
               CameraUpdate.newLatLng(result),
             );
-
-            // Add a circle denoting current user location
+            print("created");
             await controller.addCircle(
               CircleOptions(
                 circleRadius: 8.0,
                 circleColor: '#006992',
                 circleOpacity: 0.8,
-
-                // YOU NEED TO PROVIDE THIS FIELD!!!
-                // Otherwise, you'll get a silent exception somewhere in the stack
-                // trace, but the parameter is never marked as @required, so you'll
-                // never know unless you check the stack trace
                 geometry: result,
                 draggable: false,
               ),
             );
+            print("created");
           }),
     );
   }
